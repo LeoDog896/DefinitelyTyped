fcdf3531637e8a8a8a4c3d16394c3ecd128f6f6d
Tristan F <LeoDog896@hotmail.com>
2023-01-07 12:12:53 -0500

    Begin working on multiArgs property

--- types/thenify/index.d.ts
+++ types/thenify/index.d.ts
@@ -3,17 +3,33 @@
 // Definitions by: Tristan F. <https://github.com/LeoDog896>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
+type MultiArg = boolean | string[] | undefined;
+
 interface ThenifyOptions {
     withCallback?: boolean;
-    multiArgs?: boolean | string[];
+    multiArgs?: MultiArg;
 }
 
 /** De-expand the tuple if it only has one argument -- [T] becomes T, while [A, T] remains [A, T] */
 type ArgDexpansion<Arg> = Arg extends [unknown] ? Arg[0] : Arg;
 
-// Defines an interface with function overloads, that way the legacy callback support also works.
-interface ThenifyReturnFunction<Args extends unknown[], CallbackArgs extends unknown[]> {
+// boolean | string[] | undefined -> boolean | string[]
+type MultiArgNarrower<M extends MultiArg> = M extends undefined ? true : M;
+
+type ConditionalArgDexpansion<Arg, MultiArgs extends boolean | string[] | undefined> = 
+    // If multiArgs is true, de-expand the tuple when possible but leave it as a tuple if it has multiple arguments
+    MultiArgNarrower<MultiArgs> extends true ? ArgDexpansion<Arg> : 
+    // If multiArgs is false, just return the first argument
+    MultiArgNarrower<MultiArgs> extends false ? (Arg extends [unknown] ? Arg[0] : Arg) : 
+    // If multiArgs is a string[], de-expand the tuple when possible but leave it as a tuple if it has multiple arguments
+    // MultiArgNarrower<MultiArgs> extends string[] ? { [K in MultiArgNarrower<MultiArgs>[number]]: ArgDexpansion<Arg>[number] } : never;
+    never;
+interface ThenifyPromiseReturnFunction<Args extends unknown[], CallbackArgs extends unknown[], MultiArgs extends boolean | string[] | undefined> {
     (...args: Args): Promise<ArgDexpansion<CallbackArgs>>;
+}
+
+// Adds legacy callback support
+interface ThenifyReturnAllFunction<Args extends unknown[], CallbackArgs extends unknown[], MultiArgs extends boolean | string[] | undefined> extends ThenifyPromiseReturnFunction<Args, CallbackArgs, MultiArgs> {
     (...args: [...Args, (args: ArgDexpansion<CallbackArgs>) => void]): void;
 }
 
@@ -21,16 +37,28 @@ declare function thenify<
     // The function's args, ex (a, b, c, callback) will be (a, b, c)
     Args extends unknown[],
     CallbackArgs extends unknown[],
+    Options extends ThenifyOptions = ThenifyOptions
 >(
     // Extract information from the passed object:
     // ex with (a, b, c, callback), Args = [a, b, c]
     // and CallbackArgs is the arguments in the callback
     fn: (...params: [...Args, (...args: CallbackArgs) => void]) => unknown,
-    options?: ThenifyOptions
-): ThenifyReturnFunction<Args, CallbackArgs>;
+    options?: Options,
+): Options extends { withCallback: true }
+    // return promise and callback-compatible function
+    ? ThenifyReturnAllFunction<Args, CallbackArgs, Options["multiArgs"]> 
+    // only return promise-compatible function
+    : ThenifyPromiseReturnFunction<Args, CallbackArgs, Options["multiArgs"]>;
 
 export = thenify;
 
 declare namespace thenify {
-    function foo(): void;
+    function withCallback<
+        Args extends unknown[],
+        CallbackArgs extends unknown[],
+        Options extends { multiArgs?: MultiArg } = { multiArgs?: MultiArg }
+    >(
+        fn: (...params: [...Args, (...args: CallbackArgs) => void]) => unknown,
+        options?: Options
+    ): ThenifyReturnAllFunction<Args, CallbackArgs, Options["multiArgs"]>;
 }
--- types/thenify/thenify-tests.ts
+++ types/thenify/thenify-tests.ts
@@ -1,49 +1,73 @@
 import thenify = require('thenify');
+const { withCallback } = thenify;
 
-// callbacks
+// multiple args with callback
 {
-  // multiple args
-  {
-    const multiArgs = thenify((a: number, b: number, c: number, callback: (err: unknown, a: number, b: number, c: number) => void) => {
-      callback(null, a + 1, b + 1, c + 1);
-    }, { withCallback: true });
+  const multiArgs = thenify((a: number, b: number, c: number, callback: (err: unknown, a: number, b: number, c: number) => void) => {
+    callback(null, a + 1, b + 1, c + 1);
+  }, { withCallback: true });
 
-    // $ExpectType Promise<[err: unknown, a: number, b: number, c: number]>
-    multiArgs(5, 6, 7);
+  // $ExpectType Promise<[err: unknown, a: number, b: number, c: number]>
+  multiArgs(5, 6, 7);
 
-    // $ExpectType Promise<(string | number)[] | undefined>
-    multiArgs(5, 6, 7).then(([_, a, b, c]) => [a, b, c, "cat"]).catch(() => void 0);
+  // $ExpectType Promise<(string | number)[] | undefined>
+  multiArgs(5, 6, 7).then(([_, a, b, c]) => [a, b, c, "cat"]).catch(() => void 0);
 
-    // $ExpectType void
-    multiArgs(6, 7, 8, ([_, a, b, c]) => [a, b, c, "dog"]);
+  // $ExpectType void
+  multiArgs(6, 7, 8, ([_, a, b, c]) => [a, b, c, "dog"]);
 
-    // @ts-expect-error
-    multiArgs(5, 3);
-  }
+  // @ts-expect-error
+  multiArgs(5, 3);
+}
 
-  // single arg
-  {
-    const singleArg = thenify((num: number, callback: (num: number) => unknown) => {
-      callback(num / 2);
-    }, { withCallback: true });
+// single arg with callback
+{
+  const singleArg = thenify((num: number, callback: (num: number) => unknown) => {
+    callback(num / 2);
+  }, { withCallback: true });
 
-    // $ExpectType Promise<number[]>
-    singleArg(4).then(n => [n * 5]);
+  // $ExpectType Promise<number[]>
+  singleArg(4).then(n => [n * 5]);
 
-    // $ExpectType void
-    singleArg(2, x => x + 2);
-  }
+  // $ExpectType void
+  singleArg(2, x => x + 2);
+}
 
-  // no args
-  {
-    const noArg = thenify((callback: () => unknown) => {
-      callback();
-    }, { withCallback: true });
+// single arg with withCallback
+{
+  const singleArgWithCallback = withCallback((num: number, callback: (num: number) => unknown) => {
+    callback(num / 2);
+  });
 
-    // $ExpectType Promise<undefined>
-    noArg().then(() => void 0);
+  // $ExpectType Promise<number[]>
+  singleArgWithCallback(4).then(n => [n * 5]);
 
-    // $ExpectType void
-    noArg(() => void 0);
-  }
+  // $ExpectType void
+  singleArgWithCallback(2, x => x + 2);
+}
+
+// no args with callback
+{
+  const noArg = thenify((callback: () => unknown) => {
+    callback();
+  }, { withCallback: true });
+
+  // $ExpectType Promise<undefined>
+  noArg().then(() => void 0);
+
+  // $ExpectType void
+  noArg(() => void 0);
+}
+
+// no args without callback
+{
+  const noWithCallback = thenify((callback: () => void) => {
+    callback();
+  });
+
+  // $ExpectType Promise<undefined>
+  noWithCallback().then(() => void 0);
+
+  // @ts-expect-error
+  noWithCallback(() => void 0);
 }
